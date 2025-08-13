711f7fbd3061db015570b27c7c41f6897e031b3f
Eduardo Silva <edu@localhost.localdomain>
2025-08-13 12:46:21 -0300

    added: @repository on all respositorys

--- src/main/java/com/example/demo/repository/MeetingRepository.java
+++ src/main/java/com/example/demo/repository/MeetingRepository.java
@@ -2,5 +2,7 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Meeting;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MeetingRepository extends JpaRepository<Meeting, Long> {}
--- src/main/java/com/example/demo/repository/PresenceRepository.java
+++ src/main/java/com/example/demo/repository/PresenceRepository.java
@@ -3,5 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.model.Presence;
 import com.example.demo.model.PresenceId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PresenceRepository extends JpaRepository<Presence, PresenceId> {}
