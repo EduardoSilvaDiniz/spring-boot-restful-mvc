ed85902a759c7a935e91ed1f3d6eb19ef757e7fc
Eduardo Silva <eduardofox989@protonmail.com>
2025-08-20 11:43:45 -0300

    Test

--- src/main/java/com/example/demo/SystemApplication.java
+++ src/main/java/com/example/demo/SystemApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SystemApplication {
   public static void main(String[] args) {
+		System.out.println("start http server");
     SpringApplication.run(SystemApplication.class, args);
   }
 }
