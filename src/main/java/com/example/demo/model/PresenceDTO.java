5ea7bbfe4f0468bacc03837218a8f422069d2a51
Eduardo Silva <eduardofox989@protonmail.com>
2025-08-22 18:11:40 -0300

    added: presenceDTO

--- src/main/java/com/example/demo/model/PresenceDTO.java
+++ src/main/java/com/example/demo/model/PresenceDTO.java
@@ -6,20 +6,19 @@ import lombok.Setter;
 @Getter
 @Setter
 public class PresenceDTO {
-    private Long numberCard;
-    private Long meetingId;
-    private Boolean isPresence;
+  private Long numberCard;
+  private Long meetingId;
+  private Boolean isPresence;
 
-    public PresenceDTO() {
-		}
+  public PresenceDTO() {}
 
-		public PresenceDTO(Long numberCard, Long meetingId, Boolean isPresence) {
-			this.numberCard = numberCard;
-			this.meetingId = meetingId;
-			this.isPresence = isPresence;
-		}
+  public PresenceDTO(Long numberCard, Long meetingId, Boolean isPresence) {
+    this.numberCard = numberCard;
+    this.meetingId = meetingId;
+    this.isPresence = isPresence;
+  }
 
-		public Presence toEntity() {
-        return new Presence(isPresence, numberCard, meetingId);
-    }
+  public Presence toEntity() {
+    return new Presence(isPresence, numberCard, meetingId);
+  }
 }
